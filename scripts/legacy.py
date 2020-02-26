9e56bb9e1ee6ab4c5b09aaa283b5828126efa2be
Juan Pablo Consuegra Ayala <jpconsuegra93@gmail.com>
2020-02-26 17:36:44 +0100

    Implement scripts.legacy module

---
+++ scripts/legacy.py
@@ -0,0 +1,83 @@
+import bisect
+import warnings
+from pathlib import Path
+
+from scripts.utils import Collection, Keyphrase, Relation, Sentence
+
+
+class eHealth2019:
+    @classmethod
+    def load_input(cls, collection: Collection, finput: Path):
+        sentences = [s.strip() for s in finput.open(encoding="utf8").readlines() if s]
+        sentences_obj = [Sentence(text) for text in sentences]
+        collection.sentences.extend(sentences_obj)
+
+    @classmethod
+    def load_keyphrases(cls, collection: Collection, finput: Path):
+        cls.load_input(collection, finput)
+
+        input_a_file = finput.parent / ("output_a_" + finput.name.split("_")[1])
+
+        sentences_length = [len(s.text) for s in collection.sentences]
+        for i in range(1, len(sentences_length)):
+            sentences_length[i] += sentences_length[i - 1] + 1
+
+        sentence_by_id = {}
+
+        for line in input_a_file.open(encoding="utf8").readlines():
+            lid, spans, label, _ = line.strip().split("\t")
+            lid = int(lid)
+
+            spans = [s.split() for s in spans.split(";")]
+            spans = [(int(start), int(end)) for start, end in spans]
+
+            # find the sentence where this annotation is
+            i = bisect.bisect(sentences_length, spans[0][0])
+            # correct the annotation spans
+            if i > 0:
+                spans = [
+                    (
+                        start - sentences_length[i - 1] - 1,
+                        end - sentences_length[i - 1] - 1,
+                    )
+                    for start, end in spans
+                ]
+                spans.sort(key=lambda t: t[0])
+            # store the annotation in the corresponding sentence
+            the_sentence = collection.sentences[i]
+            keyphrase = Keyphrase(the_sentence, label, lid, spans)
+            the_sentence.keyphrases.append(keyphrase)
+
+            if len(keyphrase.spans) == 1:
+                keyphrase.split()
+
+            sentence_by_id[lid] = the_sentence
+
+        return sentence_by_id
+
+    @classmethod
+    def load(cls, collection: Collection, finput: Path):
+        input_b_file = finput.parent / ("output_b_" + finput.name.split("_")[1])
+
+        sentence_by_id = cls.load_keyphrases(collection, finput)
+
+        for line in input_b_file.open(encoding="utf8").readlines():
+            label, src, dst = line.strip().split("\t")
+            src, dst = int(src), int(dst)
+
+            the_sentence = sentence_by_id[src]
+
+            if the_sentence != sentence_by_id[dst]:
+                warnings.warn(
+                    "In file '%s' relation '%s' between %i and %i crosses sentence boundaries and has been ignored."
+                    % (finput, label, src, dst)
+                )
+                continue
+
+            assert sentence_by_id[dst] == the_sentence
+
+            the_sentence.relations.append(
+                Relation(the_sentence, src, dst, label.lower())
+            )
+
+        return collection
--- scripts/update.py
+++ scripts/update.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 
 from scripts.utils import Collection
+from scripts.legacy import eHealth2019
 
 
 def filter(collection: Collection, sentences):
@@ -16,7 +17,7 @@ def filter(collection: Collection, sentences):
     return Collection([find(text) for text in sentences])
 
 
-def main(path2sentences, path2corpus, path2output):
+def load_and_dump_from_corpus(path2sentences, path2corpus, path2output):
     sentences = path2sentences.read_text().splitlines()
     print(len(sentences))
     collection = Collection().load_dir(path2corpus, legacy=True, attributes=False)
@@ -26,13 +27,41 @@ def main(path2sentences, path2corpus, path2output):
     collection.dump(path2output)
 
 
-if __name__ == "__main__":
-    path2sentences = Path(sys.argv[1])
-    path2corpus = Path(sys.argv[2])
-    path2output = Path(sys.argv[3])
+def from_corpus():
+    path2sentences = Path(sys.argv[2])
+    path2corpus = Path(sys.argv[3])
+    path2output = Path(sys.argv[4])
 
     for source in path2sentences.iterdir():
         if source.is_dir():
             finput = next(source.glob("input_*.txt"))
             print(finput)
-            main(finput, path2corpus, path2output / finput.name)
+            load_and_dump_from_corpus(finput, path2corpus, path2output / finput.name)
+
+
+def load_and_dump_from_scenario(finput: Path, foutput: Path):
+    c = Collection()
+    eHealth2019.load(c, finput).dump(foutput)
+
+
+def from_scenario():
+    path2input = Path(sys.argv[2])
+    path2output = Path(sys.argv[3])
+
+    if path2input.is_dir():
+        for source in path2input.iterdir():
+            finput = next(source.glob("input_*.txt"))
+            print(finput)
+            load_and_dump_from_scenario(finput, path2output / source.name)
+    else:
+        load_and_dump_from_scenario(path2input, path2output)
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == "--scenario":
+        from_scenario()
+    elif sys.argv[1] == "--corpus":
+        from_corpus()
+    else:
+        raise Exception("Expected `--scenario` or `--corpus`.")
+
